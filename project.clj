ba718de66bd772c7d9acfb769e949c6659ce3998
Colin Jones <trptcolin@gmail.com>
2014-08-08 10:28:08 -0500

    Bump version for internal deploy

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject raven-clj "1.0.2"
+(defproject com.rentpath/raven-clj "1.0.2.1"
   :description "Sentry clojure client"
   :url "http://github.com/sethtrain/raven-clj"
   :license {:name "Eclipse Public License"
