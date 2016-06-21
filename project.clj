fb1498f325c4cf3ff822e9c733f4219c7ec21745
Jenkins Continous Integration <jenkins@jenkins-slave-02.ci.atl.primedia.com>
2016-06-21 11:46:51 -0400

    Version 1.0.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.rentpath/raven-clj "1.0.3"
+(defproject com.rentpath/raven-clj "1.0.4-SNAPSHOT"
   :description "Sentry clojure client"
   :url "http://github.com/sethtrain/raven-clj"
   :license {:name "Eclipse Public License"
