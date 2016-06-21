efe20c7716009784a1aed9d4630749c53ec9bc86
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2016-06-21 11:45:20 -0400

    [DG][000000] Sync project config <-> Jenkins build

--- project.clj
+++ project.clj
@@ -5,4 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[cheshire "5.0.1"]
                  [clj-http "0.6.4"]
-                 [org.clojure/clojure "1.5.1"]])
+                 [org.clojure/clojure "1.5.1"]]
+  :repositories [["releases" {:url "http://nexus.idg.primedia.com/nexus/content/repositories/primedia"
+                              :sign-releases false
+                              :username [:gpg :env/nexus_username]
+                              :password [:gpg :env/nexus_password]}]])
